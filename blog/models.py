d4b21e69dba9186638aff8d48bee707216b0dc59
wassim-jani <jani000wassim@gmail.com>
2024-12-09 22:40:56 +0100

    added admin user and created models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create your models here. each class here will be its own table in the DB.
+# And each attribute is going to be a different field in the DB.
+
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    date_posted=models.DateTimeField(default=timezone.now)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
