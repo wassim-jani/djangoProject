8c44cbd1c974171d6fd580031e45268230bbfd4a
wassim-jani <jani000wassim@gmail.com>
2024-12-13 22:35:05 +0100

    added the login and logout templates and routes

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = '/static/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
