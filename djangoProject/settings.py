00f242a51cc831d002ec6c1832b1bb194d0780d6
wassim-jani <jani000wassim@gmail.com>
2024-12-15 16:49:35 +0100

    added the Profile model

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+MEDIA_URL='/media/'
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
