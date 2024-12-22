c7eeadc9e6da6214b09011d683ee466668e683fc
wassim-jani <jani000wassim@gmail.com>
2024-12-22 23:12:25 +0100

    added the feature to add, update, view detail and delete posts while a the user is logged in.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here. each class here will be its own table in the DB.
@@ -14,3 +15,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -131,6 +131,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
